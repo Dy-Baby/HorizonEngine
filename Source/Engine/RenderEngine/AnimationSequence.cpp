faa856b96248fd04c884b646146d67a7a900b362
harukumo <harukumo@outlook.com>
2022-08-04 23:45:50 +0800

    Add Animation Sequence

---
+++ Source/Engine/RenderEngine/AnimationSequence.cpp
@@ -0,0 +1,16 @@
+#include "AnimationSequence.h"
+
+namespace HE
+{
+	AnimationSequence::AnimationSequence()
+		: timeLength(0)
+		, numChannels(0)
+	{
+
+	}
+	
+	AnimationSequence::~AnimationSequence()
+	{
+		
+	}
+}
